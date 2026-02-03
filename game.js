922386237918a8df84fc85a4195ad82ff4a89c81
Dipu Kumar <dipukumar69420@gmail.com>
2026-02-03 23:31:21 +0530

    update

--- game.js
+++ game.js
@@ -9,33 +9,17 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "inside which HTML element do we put the javascript ??",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    },
-    {
-        question: "what is the correct syntax for refering to external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx,js'>",
-        answer: 3
-    },
-    {
-        question: "How do write 'hello world' in an alert box?",
-        choice1: "msgbox('hello world');",
-        choice2: "alertbox('hello world');",
-        choice3: "msg('hello world');",
-        choice4: "alert('hello world');",
-        answer: 4
-    }
-];
+let questions = [];
 
+fetch("questions.json")
+.then( res => {
+    return res.json();
+})
+.then( loadedQuestions => {
+    console.log(loadedQuestions);
+    questions = loadedQuestions;
+    startGame();
+});
 
 // constants
 const CORRECT_BONUS = 10;
@@ -99,4 +83,3 @@ incrementScore = num => {
     scoreText.innerText = score;
 }
 
-startGame();
